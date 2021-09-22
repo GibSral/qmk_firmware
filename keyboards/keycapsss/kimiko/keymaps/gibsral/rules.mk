9ddcccb63893f8833cb4aa10326a3229af6e9725
Lars <LarsBaldes@gmail.com>
2021-09-22 23:35:43 +0200

    first config

---
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/rules.mk
@@ -0,0 +1,9 @@
+OLED_ENABLE = true
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = false       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CONSOLE_ENABLE = yes
+# LTO: Link Time Optimizations.
+# Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
+# This does not affect QMK Macros and Layers
+LTO_ENABLE = yes
