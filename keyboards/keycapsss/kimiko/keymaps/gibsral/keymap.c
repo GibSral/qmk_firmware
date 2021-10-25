866b808c7abdaed7eff2ce0c2af0e44f7c520887
Lars Baldes <lb@wps.de>
2021-10-25 12:33:21 +0200

    shift und enter vertauscht

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|                    |------+------+------+------+------+-----------|
  * |   ESC   |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '        |
  * |---------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+-----------|
- * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |   RCTRL   |
+ * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL/Ent   |
  * `--------------------------------------------|      /      \      \-----------------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ Enter | RSFT | RAISE  | RALT | APP  |
+ *               | WIN  | LALT | LOWER | LSFT | Space /        \ RSFT | Enter | RAISE  | RALT | APP  |
  *                `----------------------------------'          '------------------------------------'
  */
 
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCTL,
-                      KC_LGUI, KC_LALT, LOWER, KC_LSFT,    KC_SPC,   KC_ENT,  KC_RSFT,   RAISE, KC_RALT, KC_APP
+    KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RCTL_T(KC_ENT),
+                      KC_LGUI, KC_LALT, LOWER, KC_LSFT,    KC_SPC,   KC_RSFT,  KC_ENT,   RAISE, KC_RALT, KC_APP
 ),
 
 /* LOWER
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
  * |      |A-TAB |A-TAB | End  |PgDown|  APP |-------|    |-------| APP  | COPY |INSERT| QUAKE| BSLS |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ Enter | RSFT | RAISE  | RALT | APP  |
+ *               | WIN  | LALT | LOWER | LSFT | Space /        \ RSFT | Enter | RAISE  | RALT | APP  |
  *                `----------------------------------'          '------------------------------------'
  */
 
