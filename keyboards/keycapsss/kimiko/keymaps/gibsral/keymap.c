232853b89452209880eaf7313b51b95210089db7
Lars Baldes <lb@wps.de>
2021-10-30 11:54:20 +0200

    moved alt more to center

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|                    |------+------+------+------+------+-----------|
  * |   ESC   |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '/RSHIFT |
  * |---------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+-----------|
- * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL/Ent   |
- * `--------------------------------------------|      /      \      \-----------------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ RSFT | Enter | RAISE  | RALT | APP  |
+ * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL/Ent  |
+ * `--------------------------------------------|      /      \      \----------------------------------------------'
+ *               | WIN  | LOWER | LALT | LOWER | Space/        \ RSFT | RAISE | RALT  | RAISE | APP  |
  *                `----------------------------------'          '------------------------------------'
  */
 
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     LSFT_T(KC_ESC),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_QUOT),
     KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RCTL_T(KC_ENT),
-                      KC_LGUI, KC_LALT, LOWER, LOWER, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   RAISE,   RAISE, KC_RALT, KC_APP
+                      KC_LGUI, LOWER, KC_LALT, LOWER, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   RAISE,   KC_RALT, RAISE, KC_APP
 ),
 
 /* LOWER
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
  * |      |A-TAB |A-TAB | End  |PgDown|  APP |-------|    |-------| APP  | COPY |INSERT| QUAKE| BSLS |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ RSFT | Enter | RAISE  | RALT | APP  |
- *                `----------------------------------'          '------------------------------------'
+ *             | WIN  | LOWER | LALT | LOWER | Space/       \ RSFT | RAISE | RALT  | RAISE | APP  |
+ *              `----------------------------------'         '------------------------------------'
  */
 
 [_LOWER] = LAYOUT(
@@ -93,8 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   {   |    |   }   |------+------+------+------+------+------|
  * |        |A-TAB |   ä  |  ü   |  ö   |  ß   |-------|    |-------|  MB1 |  MB2 | MB3  |      |      |      |
  * `-------------------------------------------|       /     \      \-----------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ Enter | RSFT | RAISE  | RALT | APP  |
- *                `----------------------------------'          '------------------------------------'
+ *               | WIN  | LOWER | LALT | LOWER | Space/       \ RSFT | RAISE | RALT  | RAISE | APP  |
+ *                `----------------------------------'         '------------------------------------'
  */
 
 [_RAISE] = LAYOUT(
@@ -114,8 +114,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *               | WIN  | LALT | LOWER | LSFT | Space /        \ Enter | RSFT | RAISE  | RALT | APP  |
- *                `----------------------------------'          '------------------------------------'
+ *             | WIN  | LOWER | LALT | LOWER | Space/       \ RSFT | RAISE | RALT  | RAISE | APP  |
+ *              `----------------------------------'         '------------------------------------'
  */
 
 [_ADJUST] = LAYOUT(
