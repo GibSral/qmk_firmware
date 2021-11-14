8895e18fdd7aed4ad16a3207d186d3cb35c170c5
Lars Baldes <lb@wps.de>
2021-11-14 19:49:19 +0100

    Mouse Vim added as comment

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -173,6 +173,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX,  KC_END,  KC_PGDN, XXXXXXX,   BACK,    FORW,   KC_BTN1, KC_BTN2, XXXXXXX, KC_BTN3, _______, _______,
                       _______, _______, _______, _______, KC_BTN3, KC_BTN1, _______, _______, _______, _______
  )
+// Mouse VIM
+//[_MOUSE] = LAYOUT(
+//    XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+//    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                    XXXXXXX,    BACK, XXXXXXX,    FORW, XXXXXXX,  KC_F12,
+//    _______, XXXXXXX, XXXXXXX, KC_HOME,  KC_PGUP, KC_DELETE,                  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+//    XXXXXXX, XXXXXXX, XXXXXXX,  KC_END,  KC_PGDN, XXXXXXX,   BACK,    FORW,   KC_BTN1, KC_BTN2, XXXXXXX, KC_BTN3, _______, _______,
+//                      _______, _______, _______, _______, KC_BTN3, KC_BTN1, _______, _______, _______, _______
+// )
 };
 
 
