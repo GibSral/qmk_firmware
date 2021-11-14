8f81b35001e31aa814e7e6fcaba4cb2fd9acd8e7
Lars Baldes <lb@wps.de>
2021-11-14 20:41:15 +0100

    ficesd f keys in gaming layer

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -147,10 +147,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_GAME] = LAYOUT(
     KC_GRV,      KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_F6,   KC_F7,   KC_F8, XXXXXXX,  KC_F10,  KC_F11,
-    KC_TAB,     KC_F6,   KC_Q,    KC_W,    KC_E,    KC_R,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F12,
+    KC_TAB,     KC_F1,   KC_Q,    KC_W,    KC_E,    KC_R,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F12,
     KC_ESC, KC_LSHIFT,   KC_A,    KC_S,    KC_D,    KC_F,                         KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
-    KC_LCTRL,   KC_F7,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_F5,      KC_F9,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                           KC_LGUI, LOWER, KC_LALT,  KC_F8,   KC_SPC,   KC_ENT,   RAISE,   KC_RALT,   GAME, KC_APP
+    KC_LCTRL,   KC_F2,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_F5,      KC_F9,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                           KC_LGUI, LOWER, KC_LALT,  KC_F3,   KC_SPC,   KC_ENT,   RAISE,   KC_RALT,   GAME, KC_APP
  ),
 /* MOUSE
  * ,-------------------------------------------.                    ,-----------------------------------------.
