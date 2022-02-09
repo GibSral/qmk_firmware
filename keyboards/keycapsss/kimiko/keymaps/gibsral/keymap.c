2c768528d84f8a57ee7051250262d3f87bc1f100
Lars Baldes <lb@wps.de>
2022-02-09 22:00:16 +0100

    enabled leds

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+-----------|
  * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL/Ent  |
  * `--------------------------------------------|      /      \      \----------------------------------------------'
- *               | WIN  | LOWER | LALT | LOWER | Space/        \ RSFT | RAISE | RALT  | RAISE | APP  |
+ *               | WIN  | Mouse | LALT | LOWER | Space/        \ RSFT | RAISE | RALT  | Mouse | APP  |
  *                `----------------------------------'          '------------------------------------'
  */
 
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
       KC_ESC, KC_A,   KC_S,    KC_D, F_LOWER,    KC_G,                            KC_H, J_RAISE,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCTRL,
-                      KC_LGUI, TG(_MOUSE), KC_LALT, LOWER, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   RAISE,   KC_RALT, LOWER, KC_APP
+                      KC_LGUI, TG(_MOUSE), KC_LALT, LOWER, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   RAISE,   KC_LALT, TG(_MOUSE), KC_APP
 ),
 
 /* LOWER
@@ -109,9 +109,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
     _______,   KC_F1,   KC_F2,    KC_F3,  KC_F4,    KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-    _______,    KC_1,    KC_2,     KC_3,   KC_4,     KC_5,                      KC_6,    KC_7, KC_MS_U,    KC_9,    KC_0,  KC_F12,
-    _______, _______,      SS,  KC_MINS, KC_EQL,KC_DELETE,                   XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, _______,
-    _______, _______, _______,  _______, _______, _______, KC_LCBR, KC_RCBR, XXXXXXX,  M_BT_L,  M_BT_M,  B_BT_R, XXXXXXX, _______,
+    _______,    KC_1,    KC_2,     KC_3,   KC_4,     KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
+    _______, _______,      SS,  KC_MINS, KC_EQL,KC_DELETE,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, _______, _______,  _______, _______, _______, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                       _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
 ),
 /* ADJUST (Press LOWER and RAISE together)
