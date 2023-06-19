c208a75413cd6d027dc6b83792199af5f668444c
Lars Baldes <larsbaldes@gmail.com>
2023-06-19 20:14:57 +0200

    fps -> p replaced with l

--- keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/gibsral/keymap.c
@@ -168,7 +168,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Tab   |  *   |   Q  |   W  |   E  |   R  |                    |   Y  |   U  |  I   |  O   |  P   |   F9      |
  * |---------+------+------+------+------+------|                    |------+------+------+------+------+-----------|
  * |   ESC   |Shift |   A  |   S  |   D  |   F  |-------.    ,-------| Left | Down |  Up  |Right |  ;   |   '       |
- * |---------+------+------+------+------+------|  P    |    |   )   |------+------+------+------+------+-----------|
+ * |---------+------+------+------+------+------|  L    |    |   )   |------+------+------+------+------+-----------|
  * |  LCTRL  |  =   |   Z  |   X  |   C  |   V  |-------|    |-------|   N  |   M  |   <  |   >  |   /  |  RCTRL    |
  * `--------------------------------------------|      /      \      \----------------------------------------------'
  *               | ,    |   .  | LALT |   -    |Space/        \ Enter| RAISE | RALT  | RETURN | APP  |
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_PAST,   KC_Q,    KC_W,    KC_E,    KC_R,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_F9,
     KC_ESC, KC_LSHIFT,   KC_A,    KC_S,    KC_D,    KC_F,                         KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SCLN, KC_QUOT,
 
-    KC_LCTRL,KC_EQUAL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_P,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCTRL,
+    KC_LCTRL,KC_EQUAL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_L,  KC_RPRN,      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCTRL,
                            KC_COMM, KC_DOT, KC_LALT,KC_MINS,   KC_SPC,   KC_ENT,   RAISE,   KC_RALT,   FPS, KC_APP
  ),
 
